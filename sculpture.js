da5ddba004b55eb204d4c7e6656bcb7deccde8b1
Tim Rogers <tim@tim-rogers.co.uk>
2011-09-10 23:29:04 +0100

    First version including unit tests without README.md

---
+++ sculpture.js
@@ -0,0 +1,44 @@
+var Sculpture = function(title) {
+	var model = {};
+	
+	model._lastId = 0;
+	model._title = title;
+	model._collection = [];
+	
+	model.title = function() {
+		return this._title;
+	};
+	
+	model.add = function(data) {
+		// Data should be a JSON object, which we'll add a few things to
+		this._lastId = this._lastId + 1;
+		data._id = this._lastId;
+		
+		this._collection.push(data);
+		return data._id;
+		
+	};
+	model.each = function(iterator) {
+		// Pass this a function which takes a parameter of an item in the model
+		for (var i = 0; i < this._collection.length; i++) {
+			iterator(this._collection[i]);
+		}
+	};
+	model.get = function(id) {
+		// Pass this an ID of an item and it will find you that item
+		for (var i = 0; i < this._collection.length; i++) {
+			if (this._collection[i]._id == id) {
+				return this._collection[i];
+			}
+		}
+	};
+	model.first = function() {
+		return this._collection[0];
+	};
+	model.last = function() {
+		var previous = this._collection.length-1;
+		return this._collection[previous];
+	};
+	
+	return model;
+};
---
+++ tests/tests.js
@@ -0,0 +1,62 @@
+test("Ensure operation of QUnit", function() {
+	equal("string", "string", "We expect value to be 'string'");
+});
+
+module("Method tests");
+
+test("Does Sculpture.title() work?", function() {
+	var tests = Sculpture("Tests");
+	equal("Tests", tests.title(), "We expect values to be 'Tests'");
+});
+
+test("Does Sculpture.add() work?", function() {
+	var tests = Sculpture("Tests");
+	var newtest = tests.add({'title': 'Sample title'});
+	equal(1, newtest, "The result of the add function was the number 1, expected as this is the first record in the model.");
+	var result = tests.get(newtest);
+	equal('Sample title', result.title, "Getting the result back from the model worked as expected and had the correct data");
+});
+
+test("Does Sculpture.get() work?", function() {
+	var tests = Sculpture("Tests");
+	var one = { 'title': 'one' };
+	var two = { 'title': 'two' };
+	var one_id = tests.add(one);
+	var two_id = tests.add(two);
+	equal(one.title, tests.get(1).title, "The 'title' attribute of the returned object should be one, as in the first object that was added.");
+});
+
+test("Does Sculpture.first() work?", function() {
+	var tests = Sculpture("Tests");
+	var one = { 'title': 'one' };
+	var two = { 'title': 'two' };
+	var three = { 'title': 'three' };
+	tests.add(one); tests.add(two); tests.add(three);
+	equal(one.title, tests.first().title, "The title attribute of the result should be equal to that of the first record added");
+});
+
+test("Does Sculpture.last() work?", function() {
+	var tests = Sculpture("Tests");
+	var one = { 'title': 'one' };
+	var two = { 'title': 'two' };
+	var three = { 'title': 'three' };
+	tests.add(one); tests.add(two); tests.add(three);
+	equal(three.title, tests.last().title, "The title attribute of the result should be equal to that of the last record added");
+});
+
+test("Does Sculpture.each() work?", function() {
+	var tests = Sculpture("Tests");
+	var one = { 'title': 'one' };
+	var two = { 'title': 'two' };
+	var three = { 'title': 'three' };
+	tests.add(one); tests.add(two); tests.add(three);
+	records = [];
+	stringtest = true;
+	tests.each(function(data) {
+		if (typeof(data.title) != 'string') { stringtest = false; }
+		records.push(data);
+	});
+	equal(true, stringtest, "The titles of the objects added all were as expected when running through the each() loop.");
+	equal(3, records.length, "We added three records to the model and so we would expect there to be three calls to the each() function's iterator.");
+})
+
